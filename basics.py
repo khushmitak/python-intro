893a18d2de08beafeb1f83bf670deb90467b321b
Khushmita Kaur <khushp9894@gmail.com>
2024-05-02 23:08:46 -0700

    variables

--- basics.py
+++ basics.py
@@ -1,6 +1,12 @@
 print("Hello")
+
+# printing shapes
 print("     /|")
 print("    / |")
 print("   /  |")
 print("  /   |")
 print(" /____|")
+
+# variables and data types
+character_subject = "CS"
+print("I love " + character_subject + " !!")

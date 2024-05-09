506c98cacfe71ab1d25a12509055110d6dfe13ca
Khushmita Kaur <khushp9894@gmail.com>
2024-05-09 13:48:50 -0700

    comparisons

--- basics.py
+++ basics.py
@@ -112,3 +112,14 @@ if is_male or is_tall:
     print("You are a male or tall or both.")
 else:
     print("You are neither a male nor tall.")
+    
+#if statements and comparisons
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >=num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(3,4,5))

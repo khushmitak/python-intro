69ddfcff0b3323460c1893865b43ce6b6c8ce7d2
Khushmita Kaur <khushp9894@gmail.com>
2024-05-12 23:06:00 -0700

    dictionary

--- basics.py
+++ basics.py
@@ -139,3 +139,19 @@ elif op == "/":
     print(num1 / num2)
 else:
     print("Invalid operator")
+
+#dictionaries
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
